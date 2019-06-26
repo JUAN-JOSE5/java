fb4a3afc52b9e043bb91ca4eb78047607b342227
juan jose millan corzo <juanmc32145@gmail.com>
2019-06-26 14:19:30 -0500

    casi nd

--- Colegios.java
+++ Colegios.java
@@ -5,35 +5,38 @@ import java.util.Scanner;
 
 public class Colegios {
     public static void main(String[] args) {
-       Scanner leer = new Scanner(System.in);
+         Scanner leer = new Scanner(System.in);
         ArrayList <String> estudiante;
-        estudiante = new ArrayList();
+        estudiante = new ArrayList();      
        int estu;
        int n[];
-       double pro=0,not,apro=0,est;
+       double pro=0,not,apro=0,est,tot;
        System.out.println("Digite el numero de estudiantes");
         estu=leer.nextInt();
         n= new int [estu];
         for(int i=1; i<=estu; i++){
-         for(int j=1; j<=estu; j++){
-              System.out.println("Digite el nombre del estudiante "+j);
+              System.out.println("Digite el nombre del estudiante "+i);
             estudiante.add(leer.next());
-            System.out.println("Digite la nota del estudiante "+j);
+            System.out.println("Digite la nota del estudiante "+i);
             not=leer.nextDouble();
             pro=pro+not;
-            if(not>=3){
-                apro=apro+1;
+            if(not<=3){
+              apro=apro+1;
+         }        
+     }                              
+        for( int u=0; u<estudiante.size(); u++){
+         System.out.println(estudiante.get(u));
          }
-        
-     }
-         for(int j=1;j<estu;j++){
-             System.out.println(estudiante);
-             
-         }
-         System.out.println("El promedio de los estudiantes es de: "+pro);
-         System.out.println("El total de estudiantes con mas de 3 es de: "+pro);
-        }
-         }
-     }
+        System.out.println();
+          tot=pro/n[estu];
+         System.out.println("El promedio de los estudiantes es de: "+tot);
+         System.out.println("El total de estudiantes con mas de 3 es de: "+apro);
+    
+    
+
+}
+    }
+
+
  
  
